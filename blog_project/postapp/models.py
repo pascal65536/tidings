c3249a16bd6d9f37cfea576a03f960a0747bc9ff
pascal65536 <pascal65536@gmail.com>
2019-07-07 22:55:22 +0700

    PIL

--- blog_project/postapp/models.py
+++ blog_project/postapp/models.py
@@ -1,10 +1,10 @@
 import os
-
+from blog_project.settings import MEDIA_ROOT, MEDIA_URL
 from ckeditor.fields import RichTextField
-from django.conf.global_settings import MEDIA_URL
 from django.db import models
 from django.db.models.functions import datetime
 from blog_project import utils
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 
 def latin_filename(instance, filename):
@@ -17,6 +17,34 @@ def latin_filename(instance, filename):
     return format('{}/{}/{}-{}.{}'.format('blog_picture', f_folder, f_name, salt, f_ext))
 
 
+def opengraph(instance):
+    # configuration
+    font_size = 36
+    height = 480
+    width = 640
+    background_color = (255, 255, 255)
+    font_color = (0, 0, 0)
+    unicode_text = instance.title
+    image = Image.new("RGB", (width, height), background_color)
+    draw = ImageDraw.Draw(image)
+    unicode_font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+
+    textwidth, textheight = draw.textsize(unicode_text, font=unicode_font)
+    texttop = (height - textheight) // 2
+    textleft = (width - textwidth) // 2
+
+    draw.text((textleft, texttop), unicode_text, font=unicode_font, fill=font_color)
+
+    # Создадим путь и имя файла
+    f_folder = os.path.join(MEDIA_ROOT, 'opengraph', 'post')
+    salt = '{:%M%S}'.format(instance.date_post)
+    f_name = str(instance.id)
+    f_ext = 'png'
+    filename = '{}-{}.{}'.format(f_name, salt, f_ext)
+    image.save('{}/{}'.format(f_folder, filename))
+    return filename
+
+
 class Post(models.Model):
     title = models.CharField(verbose_name='Заголовок поста', max_length=255)
     lead = RichTextField(verbose_name='Лиер-абзац',)
@@ -24,6 +52,7 @@ class Post(models.Model):
     date_post = models.DateTimeField(verbose_name='Дата публикации',
                                      default=datetime.datetime.now())
     picture = models.ImageField(verbose_name='Картинка для привлечения внимания', upload_to=latin_filename, blank=True)
+    og_picture = models.CharField(verbose_name='Картинка для соцсетей', max_length=255, blank=True)
 
     # txt_doc_id = models.IntegerField()
     # txt_node_id = models.IntegerField()
@@ -32,6 +61,10 @@ class Post(models.Model):
     # created = models.DateTimeField(blank=True, null=False)
     # changed = models.DateTimeField(blank=True, null=False)
     # deleted = models.DateTimeField(blank=True, null=False)
+
+    def save(self, *args, **kwargs):
+        self.og_picture = opengraph(self)
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         # db_table = 'post'
