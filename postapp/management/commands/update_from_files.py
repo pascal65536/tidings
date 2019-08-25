899bee3f448f6fa6d6ca163356a0e082710d6adb
pascal65536 <pascal65536@gmail.com>
2019-08-26 02:32:33 +0700

    WIP

--- postapp/management/commands/update_from_files.py
+++ postapp/management/commands/update_from_files.py
@@ -1,9 +1,17 @@
 from django.core.management import BaseCommand
-import os
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        tree = os.walk('/home/pascal65536/Yandex.Disk/txt prototype24/prototype24')
-        for tr in tree:
-            print(tr)
+
+        import ipdb; ipdb.set_trace()
+
+        file_name = '/home/pascal65536/Загрузки/нюанс.rtf'
+        with open(file_name, 'r') as file:
+            for rtf in file:
+                print(rtf)
+
+        # tree = os.walk('/home/pascal65536/Yandex.Disk/txt_rtf')
+        # for tr in tree:
+        #     # len(tr[len(tr)-1])
+        #     print(tr)
