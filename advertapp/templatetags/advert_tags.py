f7e52e6ed1e1bb0ce8fec154058f4e1410af8267
pascal65536 <pascal65536@gmail.com>
2021-03-20 03:17:22 +0700

    wip

--- advertapp/templatetags/advert_tags.py
+++ advertapp/templatetags/advert_tags.py
@@ -1,50 +1,19 @@
-import json
-import os
 import random
-import redis
 
-from django.utils import timezone
-from django.conf import settings
 from django import template
-from django.core.cache import cache
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 from advertapp.models import Advert
 from newsproject import settings
+from newsproject.defaults import RACK
+from postapp.models import Post
 
 register = template.Library()
 
 
-def zeroing_advert_counter(place):
-	r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
-	r.set(r.get(place), 0)
-	return 0
-
-
-def inc_advert_counter(place, advert_obj=None, set_advert=True):
-	r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
-	# yyyy_mm_dd|banner_name expire=30 days
-	# когда закончится месяц, то сохранять предыдущие ключи в файл
-	# пройти предыдущие 15 дней и сохранить в файл
-	# ADVERT_STATIC_FILES_PATH хранить в медиа
-	if set_advert:
-		if not advert_obj:
-			return 0
-		advert_key = f'advert:{int(advert_obj.id)}:id'
-		r.set(place, advert_key)
-	advert_key = r.get(place)
-	total_views = 0
-	if advert_key:
-		total_views = r.incr(advert_key)
-	return total_views
-
-
 @register.inclusion_tag('inc/advert_banner.html', takes_context=True)
 def get_advert(context, place='skyscraper'):
-
-	rez = cache.get(f'ads_{place}')
-	if rez:
-		inc_advert_counter(place, None, set_advert=False)
-		return rez
 
 	advert_qs = Advert.objects.for_show()
 	advert_qs = advert_qs.filter(position=place)
@@ -56,11 +25,7 @@ def get_advert(context, place='skyscraper'):
 	if place in ['content', 'top', 'bottom']:
 		orientation = 'landscape'
 
-	rez = {
+	return {
 		'advert_obj': advert_obj,
 		'orientation': orientation,
 	}
-
-	cache.set(f'ads_{place}', rez, 60 * 5)  # 5 min
-
-	return rez
