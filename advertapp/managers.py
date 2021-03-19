49081c8116679f9e1237d284b3c275130c3e3cff
pascal65536 <pascal65536@gmail.com>
2021-03-20 03:21:40 +0700

    fix

--- advertapp/managers.py
+++ advertapp/managers.py
@@ -7,7 +7,12 @@ class AdvertManager(models.Manager):
     """
     Менеджер рекламы.
     """
-    def for_show(self, place):
+    def for_show(self):
+        today = datetime.now(timezone.utc)
+        qs = self.filter(date_start__lte=today, date_stop__gte=today, deleted__isnull=True)
+        return qs
+    
+    def redis_for_show(self, place):
         today = datetime.now(timezone.utc)
         qs = self.filter(date_start__lte=today, date_stop__gte=today, deleted__isnull=True, position=place)
         return qs
